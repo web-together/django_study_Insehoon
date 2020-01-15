fa1651983096976105c2f2919aae82701e641400
oereo <dlstpgns0406@gmail.com>
2020-01-15 20:47:35 +0900

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dlstp\onedrive\desktop\3-2학기\winterstudy\django_study_insehoon\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
